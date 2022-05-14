a74c5e612781e6afb41343a87391ac342107f114
IvanDrobbot <104260851+IvanDrobbot@users.noreply.github.com>
2022-05-14 21:25:34 +0300

    Add files via upload

--- assets/js/unmin/main.js
+++ assets/js/unmin/main.js
@@ -1,14 +1,27 @@
 const burger = document.querySelector(".burger");
 const burgerMenu = document.querySelector(".burger-menu");
 const modal = document.querySelector(".modal");
+const modal2 = document.querySelector(".modal-2");
 const checker = document.querySelector("#checker");
 const checkError = document.querySelector(".check-error");
 const checkBtn = document.querySelector(".check-btn");
 const checkInput = document.querySelector(".check-input");
 const checkForm = document.querySelector(".check-form");
+const checker2 = document.querySelector("#checker2");
+const checkError2 = document.querySelector(".check-error2");
+const checkBtn2 = document.querySelector(".check-btn2");
+const checkInput2 = document.querySelector(".check-input2");
+const checkForm2 = document.querySelector(".check-form2");
+const widget = document.querySelector(".widget");
+
+
 
 const openModal = document.querySelectorAll(".open-modal");
 const closeModal = document.querySelectorAll(".close-modal");
+const openModal2 = document.querySelectorAll(".open-modal-2");
+const closeModal2 = document.querySelectorAll(".close-modal-2");
+const openWidget = document.querySelectorAll(".open-widget");
+const closeWidget = document.querySelectorAll(".close-widget");
 const exAutoHeight = document.querySelectorAll(".ex-auto");
 const prodAutoHeight = document.querySelectorAll(".prod-auto");
 
@@ -16,6 +29,14 @@ const prodAutoHeight = document.querySelectorAll(".prod-auto");
 
 matchHeight(exAutoHeight);
 matchHeight(prodAutoHeight);
+
+function validateEmail(email){
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+};
 
 
 burger.addEventListener("click", () =>{
@@ -32,6 +53,17 @@ openModal.forEach(btn => {
 closeModal.forEach(btn => {
     btn.addEventListener("click", () => {
         modal.classList.remove("active");
+    });
+});
+openModal2.forEach(btn => {
+    btn.addEventListener("click", () => {
+        modal2.classList.add("active");
+    });
+});
+
+closeModal2.forEach(btn => {
+    btn.addEventListener("click", () => {
+        modal2.classList.remove("active");
     });
 });
 checkInput.addEventListener("input", () => {
@@ -67,3 +99,47 @@ checkBtn.addEventListener("click", (e) =>{
     }
 });
 
+checkInput2.addEventListener("input", () => {
+
+    if(!validateEmail(checkInput2.value)){
+        checkError2.innerHTML = "Введите корректный адрес.";
+        
+        if(checkError2.classList.contains("hidden")){
+            checkError2.classList.remove("hidden");
+        }
+    }
+    else{
+        if(!checkError2.classList.contains("hidden")){
+            checkError2.classList.add("hidden");
+        }
+    }
+});
+checkBtn2.addEventListener("mouseover", () =>{
+    if(validateEmail(checkInput2.value) && checker2.checked){
+        if(!checkBtn2.classList.contains("allow")){
+            checkBtn2.classList.add("allow");
+        }
+    }
+    else if(checkBtn2.classList.contains("allow")){
+        checkBtn2.classList.remove("allow");
+    }
+});
+
+checkBtn2.addEventListener("click", (e) =>{
+    e.preventDefault();
+
+    if(validateEmail(checkInput2.value) && checker2.checked){
+        checkForm2.submit();
+    }
+});
+
+openWidget.forEach(open => {
+    open.addEventListener("click", () => {
+        widget.classList.toggle("active");
+    });
+});
+closeWidget.forEach(close => {
+    close.addEventListener("click", () => {
+        widget.classList.toggle("active");
+    });
+});
