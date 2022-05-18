890a59d646ac2e888ba1ac269e0759c75d5c48c0
IvanDrobbot <104260851+IvanDrobbot@users.noreply.github.com>
2022-05-19 00:11:58 +0300

    Add files via upload

--- assets/js/unmin/matchheight.js
+++ assets/js/unmin/matchheight.js
@@ -6,10 +6,6 @@ function matchHeight(els){
         var arrayLength = getDivs.length;
         var heights = [];
 
-        if(getDivs.style.height.length > 2 && getDivs.style.height !== "auto"){
-            return;
-        }
-
         //Create a loop that iterates through the getDivs variable and pushes the heights of the divs into an empty array
         for (var i = 0; i < arrayLength; i++) {
             getDivs[i].style.height = "auto";
@@ -31,7 +27,38 @@ function matchHeight(els){
 
     }
 
+    var visible = function (target) {
+        // Все позиции элемента
+        var targetPosition = {
+            top: window.pageYOffset + target.getBoundingClientRect().top,
+            left: window.pageXOffset + target.getBoundingClientRect().left,
+            right: window.pageXOffset + target.getBoundingClientRect().right,
+            bottom: window.pageYOffset + target.getBoundingClientRect().bottom
+          },
+          // Получаем позиции окна
+          windowPosition = {
+            top: window.pageYOffset,
+            left: window.pageXOffset,
+            right: window.pageXOffset + document.documentElement.clientWidth,
+            bottom: window.pageYOffset + document.documentElement.clientHeight
+          };
+      
+        if (targetPosition.bottom > windowPosition.top && // Если позиция нижней части элемента больше позиции верхней чайти окна, то элемент виден сверху
+          targetPosition.top < windowPosition.bottom && // Если позиция верхней части элемента меньше позиции нижней чайти окна, то элемент виден снизу
+          targetPosition.right > windowPosition.left && // Если позиция правой стороны элемента больше позиции левой части окна, то элемент виден слева
+          targetPosition.left < windowPosition.right) { // Если позиция левой стороны элемента меньше позиции правой чайти окна, то элемент виден справа
+          return true;
+        } else {
+          return false;
+        };
+    };
+
     document.addEventListener("DOMContentLoaded", matchme);
-    document.addEventListener("scroll", matchme);
+    document.addEventListener("scroll", () => {
+        if(visible(getDivs[0])){
+            matchme();
+        }
+    });
+    
     window.addEventListener("resize", matchme);
 }
